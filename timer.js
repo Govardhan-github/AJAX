e1015a925205cbec45e337568ba207b2ba887359
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-09-03 12:03:40 +0530

    ADDED : Timer Function Added

---
+++ timer.js
@@ -0,0 +1,12 @@
+function showTime(){
+    const date = new Date;
+    return date.getHours() + "Hrs:" +date.getMinutes() +"Mins:"+ date.getSeconds() +"Secs:"; 
+}
+
+function showsessionExpire(){
+    console.log("Activity-B: Your Session expired at "+showTime());
+
+}
+console.log("Axtivity-A: Triggering Activity-B at "+showTime());
+setTimeout(showsessionExpire,5000);
+console.log("Activity-A: Triggered Activity-B at "+showTime()+" will wxcute after 5scs");
